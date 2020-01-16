db228d37c972f8a8e41be8bf94cbfe13f6f313ac
Perge, Janos <janos.perge@fmr.com>
2020-01-15 20:26:52 -0500

    add form to manually create blogs

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,25 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django import forms
+
+
+
+COLOR_CHOICES = (
+    ('green','GREEN'),
+    ('blue', 'BLUE'),
+    ('red','RED'),
+    ('orange','ORANGE'),
+    ('black','BLACK'),
+)
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text  = models.CharField(max_length=100, choices=COLOR_CHOICES, default='green')
+    # text = forms.CharField(label='Pick a color', widget=forms.Select(choices=COLOR_CHOICES))
+    #text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
